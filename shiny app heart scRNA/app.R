8c16bc826566997162bf90cea508e10ed2016d8d
rl627 <rl627@drexel.edu>
2022-08-30 21:19:16 -0400

    Sidepanel made smaller + collumn layout for figures

--- shiny app heart scRNA/app.R
+++ shiny app heart scRNA/app.R
@@ -16,70 +16,72 @@ load("data/mouse MI/all_features_MI.rdata")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("heart scRNA"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-          selectInput("dataset",
-                      "Dataset:",
-                      choices = "2019 eLife mouse MI"),
-          selectInput("gene",
-                      "Gene name:",
-                      choices = all_features,
-                      selected = "Pecam1"),
-          selectInput("cell",
-                        "Cell type:",
-                        choices = c("EC", "MP"))
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("DotPlot"),
-           plotOutput("ViolinPlot")
-        )
+  
+  # Application title
+  titlePanel("heart scRNA"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      width = 3,
+      selectInput("dataset",
+                  "Dataset:",
+                  choices = "2019 eLife mouse MI"),
+      selectInput("gene",
+                  "Gene name:",
+                  choices = all_features,
+                  selected = "Pecam1"),
+      selectInput("cell",
+                  "Cell type:",
+                  choices = c("EC", "MP"))
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      width = 9,
+      column(6, plotOutput("DotPlot")),
+      column(6, plotOutput("ViolinPlot"))
     )
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
+  
   #object name: TIP_MI_EC
   load("data/mouse MI/MI_EC_normalized.rdata")
   #TIP_MI_MP
   load("data/mouse MI/MI_MP_normalized.rdata")
   
-      output$DotPlot <- renderPlot({
-        # select object based on input
-        object_to_plot = NULL
-        if (input$cell == "EC"){object_to_plot = TIP_MI_EC} else 
-          if  (input$cell == "MP"){
-            object_to_plot = TIP_MI_MP
-          }
-        
-        DotPlot(object_to_plot,
-                features = input$gene,
-                col.min = 0, col.max = 3,
-                dot.min = 0, dot.scale = 6)+
-          theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
-        
-      
-
-    })
-      
-      output$ViolinPlot <- renderPlot({
-        # select object based on input
-        object_to_plot = NULL
-        if (input$cell == "EC"){object_to_plot = TIP_MI_EC} else 
-          if  (input$cell == "MP"){
-            object_to_plot = TIP_MI_MP
-          }
-        
-        VlnPlot(object_to_plot, features = input$gene, pt.size = 0)
-        
-      })
+  output$DotPlot <- renderPlot({
+    # select object based on input
+    object_to_plot = NULL
+    if (input$cell == "EC"){object_to_plot = TIP_MI_EC} else 
+      if  (input$cell == "MP"){
+        object_to_plot = TIP_MI_MP
+      }
+    
+    DotPlot(object_to_plot,
+            features = input$gene,
+            col.min = 0, col.max = 3,
+            dot.min = 0, dot.scale = 6)+
+      theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
+    
+    
+    
+  })
+  
+  output$ViolinPlot <- renderPlot({
+    # select object based on input
+    object_to_plot = NULL
+    if (input$cell == "EC"){object_to_plot = TIP_MI_EC} else 
+      if  (input$cell == "MP"){
+        object_to_plot = TIP_MI_MP
+      }
+    
+    VlnPlot(object_to_plot, features = input$gene, pt.size = 0)
+    
+  })
 }
 
 # Run the application 
