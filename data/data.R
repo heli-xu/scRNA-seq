9ea667e5a21798694a017251e718d84384cef3e6
heli-xu <xuheli91@gmail.com>
2022-09-18 22:19:43 -0400

    subset server_data for individual dataset for multiple shinyapps
    ui_features_all not used, copied original features from data/ instead

--- data/data.R
+++ data/data.R
@@ -2,31 +2,45 @@
 library(tidyverse)
 
 # 1. Prepare UI object -----
-load("healthy MI non myocyte cardiac/clean/all_features_MI.rdata")
-
-features_MI <- tibble(gene = all_features, dataset = "2019 eLife mouse MI")
- 
-load("GSE120064_TAC CM_NMCC_mtx_cell_info/clean/all_features_TAC.rdata")  
+{
+  load("healthy MI non myocyte cardiac/clean/all_features_MI.rdata")
   
-features_TAC <- tibble(gene = all_features_TAC, dataset = "2020 Circulation mouse TAC")
-
-ui_features <- bind_rows(features_MI, features_TAC)
+  features_MI <- tibble(gene = all_features, dataset = "2019 eLife mouse MI")
   
-save(ui_features, file = "../shiny/R/ui_features.rdata")
+  load("GSE120064_TAC CM_NMCC_mtx_cell_info/clean/all_features_TAC.rdata")  
+  
+  features_TAC <- tibble(gene = all_features_TAC, dataset = "2020 Circulation mouse TAC")
+  
+  ui_features_all <- bind_rows(features_MI, features_TAC)
+}
+##for individual dataset, all_features files were copied directly
 
-# 2. Prepare Server object ----
+# 2. Prepare Server object ----(all mice datasets)
 
 load("../data/healthy MI non myocyte cardiac/clean/MI_EC_normalized.rdata") #object name: TIP_MI_EC
 load("../data/healthy MI non myocyte cardiac/clean/MI_MP_normalized.rdata") #TIP_MI_MP
 load("../data/GSE120064_TAC CM_NMCC_mtx_cell_info/clean/TAC_EC_metadata.rdata") #TAC_EC
 load("../data/GSE120064_TAC CM_NMCC_mtx_cell_info/clean/TAC_MP_metadata.rdata") #TAC_MP
+load("../data/GSE120064_TAC CM_NMCC_mtx_cell_info/clean/TAC_CM_metadata.rdata")
 
-server_data = tibble(
+
+server_data_all = tibble(
   dataset = c("2019 eLife mouse MI", "2020 Circulation mouse TAC"),
   EC = list(TIP_MI_EC, TAC_EC),
-  MP = list(TIP_MI_MP,TAC_MP)
+  MP = list(TIP_MI_MP,TAC_MP),
+  CM = list(NA, TAC_CM)
 ) %>% 
-  pivot_longer(c(EC,MP), names_to = 'cell', values_to = 'obj')
+  pivot_longer(c(EC,MP,CM), names_to = 'cell', values_to = 'obj')
 
 
-save(server_data, file = "../shiny/R/server_data.rdata")
+pull_dataset <- function(x) {
+  filter(server_data_all, dataset == x) %>% 
+    select(-dataset)
+}
+
+server_data_MI <- pull_dataset("2019 eLife mouse MI")
+save(server_data_MI, file = "../shiny-mouseMI/R/server_data_MI.rdata")
+
+server_data_TAC <- pull_dataset("2020 Circulation mouse TAC")
+
+save(server_data_TAC, file = "../shiny-mouseTAC/R/server_data_TAC.rdata")
