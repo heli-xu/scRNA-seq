a541aeca91a71372beaea2ff2875571a30f7b785
heli-xu <xuheli91@gmail.com>
2022-09-26 11:11:01 -0400

    update server_data to include 2020Natcellbio humanHF data

--- data/data.R
+++ data/data.R
@@ -15,20 +15,22 @@ library(tidyverse)
 }
 ##for individual dataset, all_features files were copied directly
 
-# 2. Prepare Server object ----(all mice datasets)
+# 2. Prepare Server object ----(2 mice + 1 human datasets)
 
 load("../data/healthy MI non myocyte cardiac/clean/MI_EC_normalized.rdata") #object name: TIP_MI_EC
 load("../data/healthy MI non myocyte cardiac/clean/MI_MP_normalized.rdata") #TIP_MI_MP
 load("../data/GSE120064_TAC CM_NMCC_mtx_cell_info/clean/TAC_EC_metadata.rdata") #TAC_EC
 load("../data/GSE120064_TAC CM_NMCC_mtx_cell_info/clean/TAC_MP_metadata.rdata") #TAC_MP
 load("../data/GSE120064_TAC CM_NMCC_mtx_cell_info/clean/TAC_CM_metadata.rdata")
-
+load("../data/2020 nat cell biol human normal vs failing heart/clean/human_hmn_CM.rdata")
+load("../data/2020 nat cell biol human normal vs failing heart/clean/human_hmn_EC.rdata")
+load("../data/2020 nat cell biol human normal vs failing heart/clean/human_hmn_MP.rdata")
 
 server_data_all = tibble(
-  dataset = c("2019 eLife mouse MI", "2020 Circulation mouse TAC"),
-  EC = list(TIP_MI_EC, TAC_EC),
-  MP = list(TIP_MI_MP,TAC_MP),
-  CM = list(NA, TAC_CM)
+  dataset = c("2019 eLife mouse MI", "2020 Circulation mouse TAC", "2020 NatCellBio human HF"),
+  EC = list(TIP_MI_EC, TAC_EC, human_EC),
+  MP = list(TIP_MI_MP,TAC_MP, human_MP),
+  CM = list(NA, TAC_CM, human_CM)
 ) %>% 
   pivot_longer(c(EC,MP,CM), names_to = 'cell', values_to = 'obj')
 
@@ -44,3 +46,6 @@ save(server_data_MI, file = "../shiny-mouseMI/R/server_data_MI.rdata")
 server_data_TAC <- pull_dataset("2020 Circulation mouse TAC")
 
 save(server_data_TAC, file = "../shiny-mouseTAC/R/server_data_TAC.rdata")
+
+server_data_humanHF <- pull_dataset("2020 NatCellBio human HF")
+save(server_data_humanHF, file = "../shiny-humanHF/R/server_data_humanHF.rdata")
